19ee53e0b3fbf71e74a1860ac67aebe3d151a314
El Bhiruni <elbhiruni.tik@gmail.com>
2022-01-30 15:55:38 +0700

    add simple ui

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
 let computerScore = 0;
 let playerScore = 0;
+const buttons = document.querySelectorAll('button');
+const score = document.querySelector('.score');
+const result = document.querySelector('.result');
+const winner = document.querySelector('.winner');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.className;
+        let computerSelection = computerPlay();
+        if (playerScore === 5 || computerScore === 5) {
+            buttons[0].disabled = true;
+            buttons[1].disabled = true;
+            buttons[2].disabled = true;
+        }
+        playRound(playerSelection, computerSelection);
+        score.textContent = `Player ${playerScore} Computer ${computerScore}`;
+        if (playerScore === 5 && computerScore < 5) {
+            winner.textContent = "PLAYER WINN!!!";
+        }
+        if (computerScore === 5 && playerScore < 5) {
+            winner.textContent = "COMPUTER WINN!!!";
+        }
+    });
+});
 
 function computerPlay() {
     const randomNumber = Math.floor((Math.random() * 3) + 1);
-    switch(randomNumber) {
+    switch (randomNumber) {
         case 1:
             return "Rock";
             break;
-        case 2:
-            return "Paper";
-            break;
-        case 3:
+            case 2:
+                return "Paper";
+                break;
+                case 3:
             return "Scissors";
             break;
-        default:
-            return "Default";
-            break;
-    }
-}
-
-const capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-}
+            default:
+                return "Default";
+                break;
+            }
+        }
+        
+        const capitalize = (str) => {
+            return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+        }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = capitalize(playerSelection);
-
+    
     if (
         (playerSelection === "Rock" && computerSelection === "Scissors") ||
         (playerSelection === "Paper" && computerSelection === "Rock") ||
         (playerSelection === "Scissors" && computerSelection === "Paper")
-    ) {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        ) {
+        result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
         return playerScore++;
     } else if (playerSelection === computerSelection) {
-        console.log("You Tied");
+        result.textContent = `You Tide! ${computerSelection} and ${playerSelection}`;
         return playerScore += 0;
     } else {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
         return computerScore++;
     }
 }
-
-function game() {
-    for (let index = 1; index <= 5; index++) {
-        let playerSelection = prompt("Choose Rock, Paper or Scissors");
-        let computerSelection = computerPlay();
-
-        if (playerSelection === null) {
-            return "exit";
-        }
-
-        console.log(`Round ${index}`);
-        console.log(`Computer : ${computerSelection}, Player : ${playerSelection}`);
-        playRound(playerSelection, computerSelection);
-    }
-    console.log(`Score: Computer ${computerScore}, Player ${playerScore}`);
-    
-    return (playerScore > computerScore) ? "Player WIN!"
-            : (playerScore < computerScore) ? "Computer WIN!"
-            : "Tide!!!";
-}
-
-console.log(game());
